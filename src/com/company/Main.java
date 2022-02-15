45f24f2bcc245fdd3cd466ed0f674e2104c46320
Cameron <cameronhanlin@Camerons-MacBook-Pro.local>
2022-02-15 15:34:38 -0700

    Initializing repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Create a new project named Golf
+Create a new repo in Github named Golf
+In InteliJ create a new project named Golf
+In Terminal navigate to your MadLibs project location
+In terminal, use git init
+Copy the Github SSH key for your new Github repo
+In Terminal Setup the git remote named origin by using the Github SSH key you copied earlier
+Stage and commit the init changes to your local repo
+Push changes to Github
+Create a class named GolfCourse
+It should have a private property that is an array of size 18, with each index being an int value of 3, 4, or 5.
+It should have a method to compare scores that takes two int parameters (hole number and number of shots)
+The method should return an int of the amount over or under par the number of shots is for the hole number provided
+In your main class, create an int array of size 18 and populate each index with a number from 1 to 7 to simulate a golfer playing the course.
+Using your GolfCourse class, calculate the golfers score (the total number above or below par based on each holes number of shots vs the par amount for that hole)
+Print out the score to the console.
+	 */
+    }
+}
